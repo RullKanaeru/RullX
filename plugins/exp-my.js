2a0b75482351ee2b38de84b29d918a059079196a
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-14 15:09:33 +0700

    Update exp-my.js

--- plugins/exp-my.js
+++ plugins/exp-my.js
@@ -102,22 +102,22 @@ let handler = async (m, { conn }) => {
   const crates = Object.keys(inventory.crates).map(v => user[v] && `*${global.rpg.emoticon(v)}${v}:* ${user[v]}`).filter(v => v).join('\n').trim()
   const pets = Object.keys(inventory.pets).map(v => user[v] && `*${global.rpg.emoticon(v)}${v}:* ${user[v] >= inventory.pets[v] ? 'Max Levels' : `Level(s) ${user[v]}`}`).filter(v => v).join('\n').trim()
   const cooldowns = Object.entries(inventory.cooldowns).map(([cd, { name, time }]) => cd in user && `*✧ ${name}*: ${new Date() - user[cd] >= time ? '✅' : '❌'}`).filter(v => v).join('\n').trim()
-  const caption = `*🧑🏻‍🏫 ɴᴀᴍᴇ:* ${user.registered ? user.name : conn.getName(m.sender)}
+  const caption = `*📌 ɴᴀᴍᴀ:* ${user.registered ? user.name : conn.getName(m.sender)}
 *▸ ᴇxᴘ:* ${user.exp}
 *▸ ʟᴇᴠᴇʟ:* ${user.level}
 *▸ ʀᴏʟᴇ:* ${user.role}
-*▸ ʜᴇᴀʟᴛʜ:* ${user.health}
+*▸ ɴʏᴀᴡᴀ:* ${user.health}
 *▸ ʟɪᴍɪᴛ:* ${user.limit}
-*▸ ᴍᴏɴᴇʏ:* ${user.money}${user.atm ? `
+*▸ ᴜᴀɴɢ:* ${user.money}${user.atm ? `
 *▸ ᴀᴛᴍ:* ʟᴠ.${user.atm}
 *▸ ʙᴀɴᴋ:* ${user.bank} $ / ${user.fullatm} $`: ''}
 
 ▸ *sᴛᴀᴛᴜs:* ${user.premiumTime > 0 ? 'Premium' : 'Free'}
-▸ *ʀᴇɢɪsᴛᴇʀᴇᴅ:* ${user.registered ? 'Yes':'No'}${user.premiumTime >= 1 ? `
+▸ *ᴛᴇʀᴅᴀғᴛᴀʀ:* ${user.registered ? 'Yes':'No'}${user.premiumTime >= 1 ? `
 ▸ *ᴇxᴘɪʀᴇᴅ:*
 ${clockString(user.premiumTime - new Date() * 1)}`: ''}
 `.trim()
-  conn.sendButton(m.chat, `${htki} ᴜ s ᴇ ʀ s ${htka}`, caption, null, [`ɪɴᴠᴇɴᴛᴏʀʏ`, '.inv'],m)
+  conn.sendButton(m.chat, `${htki} ᴜ s ᴇ ʀ s ${htka}`, caption, null, [`ʀᴘɢ ɪɴᴠᴇɴᴛᴏʀʏ`, '.inv'],m)
 }
 handler.help = ['my']
 handler.tags = ['xp']
@@ -133,5 +133,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [ye, ' *ʏᴇᴀʀs 🗓️*\n', mo, ' *ᴍᴏɴᴛʜ 🌙*\n', d, ' *ᴅᴀʏs ☀️*\n', h, ' *ʜᴏᴜʀs 🕐*\n', m, ' *ᴍɪɴᴜᴛᴇ ⏰*\n', s, ' *sᴇᴄᴏɴᴅ ⏱️*'].map(v => v.toString().padStart(2, 0)).join('')
+  return [ye, ' *ᴛᴀʜᴜɴ 🗓️*\n', mo, ' *ʙᴜʟᴀɴ 🌙*\n', d, ' *ʜᴀʀɪ ☀️*\n', h, ' *ᴊᴀᴍ 🕐*\n', m, ' *ᴍɪɴᴜᴛᴇ ⏰*\n', s, ' *sᴇᴄᴏɴᴅ ⏱️*'].map(v => v.toString().padStart(2, 0)).join('')
 }
