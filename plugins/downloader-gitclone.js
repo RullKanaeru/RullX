e54dfc73dbb9f972a9d62f86f942c799eb149ea2
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-14 16:34:18 +0700

    Update downloader-gitclone.js

--- plugins/downloader-gitclone.js
+++ plugins/downloader-gitclone.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let handler = async (m, {conn, text, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Example user ${usedPrefix}${command} kannachann|kannabot-md`
+    if (!args[0]) throw `Example user ${usedPrefix}${command} fantasy|fantasybott`
     let [usr, rep] = text.split`/`
     let url = `https://api.github.com/repos/${encodeURIComponent(usr)}/${encodeURIComponent(rep)}/zipball`
     let name = `${encodeURIComponent(rep)}.zip`
