c01c38b67b75beac1756c76038172d86616f1c7f
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-14 15:15:02 +0700

    Update bank-cek.js

--- plugins/bank-cek.js
+++ plugins/bank-cek.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn }) => {
   let user = global.db.data.users[m.sender]
   const caption = `
-▧「 *B A N K  U S E R* 」
-│ 📛 *Name:* ${user.registered ? user.name : conn.getName(m.sender)}
-│ 💳 *Atm:* ${user.atm > 0 ? 'Level ' + user.atm : '✖️'}
-│ 🏛️ *Bank:* ${user.bank} 💲 / ${user.fullatm} 💲
-│ 💹 *Money:* ${user.money} 💲
-│ 🤖 *Robo:* ${user.robo > 0 ? 'Level ' + user.robo : '✖️'}
-│ 🌟 *Status:* ${user.premiumTime > 0 ? 'Premium' : 'Free'}
-│ 📑 *Registered:* ${user.registered ? 'Yes':'No'}
+▧「 *𝗕 𝗔 𝗡 𝗞   𝗕 𝗢 𝗧* 」
+│ 📛 *ɴᴀᴍᴀ:* ${user.registered ? user.name : conn.getName(m.sender)}
+│ 💳 *ᴀᴛᴍ:* ${user.atm > 0 ? 'Level ' + user.atm : '✖️'}
+│ 🏛️ *ʙᴀɴᴋ:* ${user.bank} 💲 / ${user.fullatm} 💲
+│ 💹 *ᴜᴀɴɢ:* ${user.money} 💲
+│ 🤖 *ʀᴏʙᴏ:* ${user.robo > 0 ? 'Level ' + user.robo : '✖️'}
+│ 🌟 *sᴛᴛᴜs:* ${user.premiumTime > 0 ? 'Premium' : 'Free'}
+│ 📑 *ᴛᴇʀᴅᴀғᴛᴀʀ:* ${user.registered ? 'Yes':'No'}
 └──···
 `.trim()
   conn.sendButton(m.chat, caption, global.wm, 'https://telegra.ph/file/0451b07945f7f9633b59b.jpg', [`Inventory`, '.inv'],m)
