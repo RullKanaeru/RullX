d10daad68138e90e64b6992fbc78173bd1e5ebdf
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-14 15:22:29 +0700

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `kalo kamu ada keluhan tentang bug/eror atau lainya klik\n\nCustomer Service\n📮Klik link berikut: https://s.id/1awyu`
+    if (!text) throw `𝑲𝒂𝒍𝒂𝒖 𝑨𝒅 𝑲𝒆𝒍𝒖𝒉𝒂𝒏 𝑳𝒂𝒏𝒈𝒔𝒖𝒏𝒈 𝑯𝒖𝒃𝒖𝒏𝒈𝒊 𝑶𝒘𝒏𝒆𝒓 𝑩𝒐𝒕`
     if (text.length < 10) throw `Laporan terlalu pendek, minimal 10 karakter!`
     if (text.length > 1000) throw `Laporan terlalu panjang, maksimal 1000 karakter!`
-    let teks = `*${command.toUpperCase()}!*\n\nDari : *@${m.sender.split`@`[0]}*\n\nPesan : ${text}\n`
+    let teks = `*${command.toUpperCase()}!*\n\nDari : *@${m.sender.split`@`[0]}*\n\n𝑷𝒆𝒔𝒂𝒏 : ${text}\n`
     conn.reply(global.nomorown + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
         contextInfo: {
             mentionedJid: [m.sender]
         }
     })
-    m.reply(`'_Pesan terkirim kepemilik bot, jika ${command.toLowerCase()} hanya main-main tidak akan ditanggapi.\n\nJangan lupa cek info di https://fangzbot.websites.co.in/'_`)
+    m.reply(`'_Pesan terkirim kepemilik bot, jika ${command.toLowerCase()} hanya main-main tidak akan ditanggapi'_`)
 }
 handler.help = ['report', 'request'].map(v => v + ' <teks>')
 handler.tags = ['info']
